d89c1b8b6e55170be3a250ba0ed5ecd32beae6cc
MohammadSina Ghanbari <sina_ghanbari@outlook.com>
2024-03-04 13:47:05 +0100

    Changes

---
+++ MaterialBalances.m
@@ -0,0 +1,17 @@
+function eqs = MaterialBalances(unk)
+% Input Data:
+x_M = 0.05; % Mole fraction of Methane
+s_b = 0.96; % Benzene Selectivity
+p_b = 265; % Benzene amount in P [kmol/h]
+HTR = 5; % Hydrogen to Toluene ratio
+
+% Unknowns:
+F1h = unk(1);
+F1m = unk(2);
+F2t = unk(3);
+INh = unk(4);
+INm = unk(5);
+INt = unk(6);
+
+
+end
