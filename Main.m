8afa7634ed2a15d296b78d4d0cbc20821609cadb
MohammadSina Ghanbari <sina_ghanbari@outlook.com>
2024-03-18 17:51:11 +0100

    Modify Main File

--- Main.m
+++ Main.m
@@ -1,7 +1,7 @@
 clearvars
 close all
 clc
-
+%% Couple Kinetics and Material Balances - 11 March 2024
 % DoFs
 Tvalues = (600:50:750)+273.15;              %[K]
 SFvalues = 0.02:0.02:1;                     %[-]
@@ -46,38 +46,38 @@ for i = 1:length(Tvalues)
 
     end
 end
-
 R = Rh + Rm;            %[kmol/h]
-
 figure(1)
 plot(SFvalues,R)
 xlabel('Split Factor [-]')
 ylabel('Recycle, R [kmol/h]')
 
+%% Plotting H2 fraction in vent stream versus Split Factor - 18 March 2024
 Vent_frac = Vh./(Vh+Vm);        % Hydrogen fraction in vent stream
-
 figure(2)
 plot(SFvalues,Vent_frac(1,:))
 hold on
-plot(SFvalues,Vent_frac(2,:))
-plot(SFvalues,Vent_frac(3,:))
-plot(SFvalues,Vent_frac(4,:))
+for i=2:4
+    plot(SFvalues,Vent_frac(i,:))
+end
 grid on
 xlabel('Split Factor'); ylabel('Vh [kmol/hr]')
 legend('873.15 K', '923.15 K', '973.15 K', '1023.15 K')
 title('Amount of Hydrogen in Vent Stream Versus Split Factor')
-%% EP Calculation - 18 March 2024
+%% EP2 Calculation - 18 March 2024
 Keys = {'Benzene','Toluene','Hydrogene','Byphenil', 'Methane'};
 Cost_sell = [12.5, 8.8, 2.1, 7.4, 2.1];
 Cost_sell_Materials = containers.Map(Keys,Cost_sell);
 delH_burn = [1.41, 1.68, 0.123, 2.688, 0.383];
 Cost_burn_Materials = containers.Map(Keys, delH_burn*4);
 
-% 1st Scenario
-EP2_sell = 8000*(Cost_sell_Materials('Benzene')*Bb + Cost_sell_Materials('Byphenil')*Dd - Cost_sell_Materials('Hydrogene')*(F1h+F1m) - Cost_sell_Materials('Toluene')*F2t + Cost_burn_Materials('Hydrogene')*Vh + Cost_burn_Materials('Methane')*Vm);
-EP2_burn = 8000*(Cost_sell_Materials('Benzene')*Bb + Cost_burn_Materials('Byphenil')*Dd - Cost_sell_Materials('Hydrogene')*(F1h+F1m) - Cost_sell_Materials('Toluene')*F2t + Cost_burn_Materials('Hydrogene')*Vh + Cost_burn_Materials('Methane')*Vm);
+% 1st Scenario [M$/8000hr]
+EP2_sell = 1e-6*8000*(Cost_sell_Materials('Benzene')*Bb + Cost_sell_Materials('Byphenil')*Dd - Cost_sell_Materials('Hydrogene')*(F1h+F1m) - Cost_sell_Materials('Toluene')*F2t + Cost_burn_Materials('Hydrogene')*Vh + Cost_burn_Materials('Methane')*Vm);
+% 2nd Scenario [M$/8000hr]
+EP2_burn = 1e-6*8000*(Cost_sell_Materials('Benzene')*Bb + Cost_burn_Materials('Byphenil')*Dd - Cost_sell_Materials('Hydrogene')*(F1h+F1m) - Cost_sell_Materials('Toluene')*F2t + Cost_burn_Materials('Hydrogene')*Vh + Cost_burn_Materials('Methane')*Vm);
+ 
 
-% 
+% Plot EP2 (both "sell" and "burn") vs. Split factor, by imposing a selectivity of at least 96%
 figure(3)
 subplot(1,2,1);
 plot(SFvalues,EP2_sell(1,:))
@@ -89,7 +89,7 @@ grid on
 xlabel('Split Factor'); ylabel('EP (M$/8000hr)')
 legend('873.15 K', '923.15 K', '973.15 K', '1023.15 K')
 title('EP2 for sell scenario')
-% figure(4)
+
 subplot(1,2,2);
 plot(SFvalues,EP2_burn(1,:))
 hold on
@@ -101,6 +101,7 @@ xlabel('Split Factor'); ylabel('EP (M$/8000hr)')
 legend('873.15 K', '923.15 K', '973.15 K', '1023.15 K')
 title('EP2 for burn scenario')
 
+% Plot  EP2 (both "sell" and "burn") vs. Temperature, as a function of the split factor
 figure(4)
 subplot(1,2,1);
 plot(Tvalues, EP2_sell(:,1))
@@ -112,7 +113,6 @@ grid on
 xlabel('Temperature [K]'); ylabel('EP (M$/8000hr)')
 title('EP2 for sell scenario')
 
-% figure(6)
 subplot(1,2,2);
 plot(Tvalues, EP2_burn(:,1))
 hold on
